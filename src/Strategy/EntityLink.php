2ed25ebbc30811d5b0ea7522fed350f036080f63
Tom H Anderson <tom.h.anderson@notevault.com>
2016-04-12 12:16:08 -0700

    Use hydration extract to get identifier for EntityLink

--- src/Strategy/EntityLink.php
+++ src/Strategy/EntityLink.php
@@ -3,11 +3,12 @@
 namespace ZF\Doctrine\Hydrator\Strategy;
 
 use Zend\Stdlib\Hydrator\Strategy\StrategyInterface;
-use Zend\Filter\FilterChain;
-use DoctrineModule\Stdlib\Hydrator\Strategy\AbstractCollectionStrategy;
-use ZF\Hal\Link\Link;
-use ZF\Hal\Entity as HalEntity;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use DoctrineModule\Stdlib\Hydrator\DoctrineObject as DoctrineHydrator;
+use Doctrine\Common\Util\ClassUtils;
 use ZF\Hal\Link\LinkCollection;
+use ZF\Hal\Entity as HalEntity;
+use ZF\Hal\Link\Link;
 use stdClass;
 
 /**
@@ -18,11 +19,32 @@ use stdClass;
 class EntityLink implements
     StrategyInterface
 {
+    protected $serviceLocator;
     protected $metadataMap;
+    protected $doctrineHydratorConfig;
 
-    public function setMetadataMap(array $map)
+    /**
+      * The service locator must be injected here so an object manager
+      * can be found for a given entity.  The object manager is used to
+      * extract the entity to assign identifier field values to the route.
+      * We cannot use reflection to get the values because the $value may
+      * be a proxy object.
+      */
+    public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
     {
-        $this->metadataMap = $map;
+        $this->serviceLocator = $serviceLocator;
+
+        return $this;
+    }
+
+    public function getServiceLocator()
+    {
+        return $this->serviceLocator;
+    }
+
+    public function setMetadataMap(array $metadataMap)
+    {
+        $this->metadataMap = $metadataMap;
 
         return $this;
     }
@@ -32,23 +54,47 @@ class EntityLink implements
         return $this->metadataMap;
     }
 
+    public function setDoctrineHydratorConfig($config)
+    {
+        $this->doctrineHydratorConfig = $config;
+
+        return $this;
+    }
+
+    public function getDoctrineHydratorConfig()
+    {
+        return $this->doctrineHydratorConfig;
+    }
+
+    /**
+     * Look up the object manager for the given entity and create a basic hydrator
+     */
+    public function getHydratorForEntity($value)
+    {
+        $entityMetadata = $this->getMetadataMap()[ClassUtils::getRealClass(get_class($value))];
+        $objectManagerClass = $this->getDoctrineHydratorConfig()[$entityMetadata['hydrator']]['object_manager'];
+
+        return new DoctrineHydrator($this->getServiceLocator()->get($objectManagerClass), true);
+    }
+
+    /**
+     * Return a HAL entity with just a self link
+     */
     public function extract($value)
     {
-        $entityMetadata = $this->getMetadataMap()[get_class($value)];
-
-        $identifierField = $entityMetadata['entity_identifier_name'];
-        $identifierFieldGetter = 'get' . $identifierField;
+        $entityValues = $this->getHydratorForEntity($value)->extract($value);
+        $entityMetadata = $this->getMetadataMap()[ClassUtils::getRealClass(get_class($value))];
 
         $link = new Link('self');
         $link->setRoute($entityMetadata['route_name']);
-        $link->setRouteParams(array($identifierField => $value->{$identifierFieldGetter}()));
+        $link->setRouteParams(array(
+            $entityMetadata['route_identifier_name'] => $entityValues[$entityMetadata['entity_identifier_name']]
+        ));
 
         $linkCollection = new LinkCollection();
         $linkCollection->add($link);
 
-        $entity = new stdClass();
-        $halEntity = new HalEntity($entity);
-
+        $halEntity = new HalEntity(new stdClass());
         $halEntity->setLinks($linkCollection);
 
         return $halEntity;
